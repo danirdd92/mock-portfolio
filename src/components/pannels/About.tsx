2e1fcf2c91d0f3e7191915be7a8ee119583b12ba
Daniel Rashba <danirdd92@gmail.com>
2023-03-04 11:24:56 +0200

    fixed paragraph spacing

--- src/components/pannels/About.tsx
+++ src/components/pannels/About.tsx
@@ -11,20 +11,27 @@ export const About = () => {
       <span className='block text-text-gray text-sub-title mb-[10px]'>
         Design & Development
       </span>
-
-      <p className='text-[#c0c0c0] text-paragraph leading-7'>
-        An AI wizard, possesses a keen intellect and a natural affinity for
-        technology. Constantly seeking out new ways to merge artificial
-        intelligence with user interface design, they are a true innovator in
-        their field. Leading a team of skilled designers and developers, they
-        use their powers for good, striving to make the world a better place
-        through exceptional user experiences. Whether it's crafting beautiful
-        apps or designing seamless websites, no challenge is too great for them
-        to tackle. With a deep understanding of the mystical arts of tech and
-        design, always on the cutting-edge, seeking out new ways to improve and
-        evolve. Join them on their journey to make a lasting impact on the
-        world, one user at a time.
-      </p>
+      <div className='text-[#c0c0c0] text-paragraph leading-7 flex flex-col gap-[10px]'>
+        <p>
+          An AI wizard, possesses a keen intellect and a natural affinity for
+          technology. Constantly seeking out new ways to merge artificial
+          intelligence with user interface design, they are a true innovator in
+          their field.
+        </p>
+        <p>
+          Leading a team of skilled designers and developers, they use their
+          powers for good, striving to make the world a better place through
+          exceptional user experiences. Whether it's crafting beautiful apps or
+          designing seamless websites, no challenge is too great for them to
+          tackle.
+        </p>
+        <p>
+          With a deep understanding of the mystical arts of tech and design,
+          always on the cutting-edge, seeking out new ways to improve and
+          evolve. Join them on their journey to make a lasting impact on the
+          world, one user at a time.
+        </p>
+      </div>
     </div>
   );
 };
